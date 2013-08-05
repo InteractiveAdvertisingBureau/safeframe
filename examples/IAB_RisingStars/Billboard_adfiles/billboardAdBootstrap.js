526f8c08da0f827db03aeab63f45c7ac68a8ee8c
Chris Cole <goosemanjack@gmail.com>
2013-08-05 16:40:33 -0700

    Commit prior to rename

--- examples/IAB_RisingStars/Billboard_adfiles/billboardAdBootstrap.js
+++ examples/IAB_RisingStars/Billboard_adfiles/billboardAdBootstrap.js
@@ -1,9 +1,8 @@
-var img = "http://safeframes.emination.com/adfiles/backupImage.png"
 // var img = "../../examples/IAB_RisingStars/Billboard_adfiles/backupImage.png";
 
-var html =  "<!-- THIS IS EMBEDDED AS scripted content -->\n<style>\n#showAdButton{\n display: none;\n cursor: pointer;\n text-decoration: underline;\n text-align: right;\n width:970px;\n color: #888;\n font-weight:bold;\n font-size:0.8em;\n font-family: Verdana;\n}\n</style>\n\n<div id=\"showAdButton\" onclick=\"expandAd()\">Show ad <img src=\"http://safeframes.emination.com/adfiles/downarrow.png\" /></div>\n\n"
-html += "<img src=\"" + img + "\"\n id=\"adImage\"\n usemap=\"admap\"\n style=\"width:970px;height:250px;\" />\n\n\n\n<map name=\"admap\">\n  <area shape=\"rect\" coords=\"850,5,965,30\" href=\"javascript:collapseAd()\" alt=\"Close\" onclick=\"collapseAd()\">\n</map>\n<!--\n<scr"+"ipt src=\"\"../../examples/IAB_RisingStars/Billboard_adfiles/adScriptBehavior.js\"></scr"+"ipt>\n-->\n\n<scr"+"ipt src=\"http://safeframes.emination.com/adfiles/adScriptBehavior.js\"></scr"+"ipt>\n"
+var img = "https://s3-us-west-2.amazonaws.com/safeframe/samples/rising_stars/billboard_adfiles/backupImage.png"
+var scr = "https://s3-us-west-2.amazonaws.com/safeframe/samples/rising_stars/billboard_adfiles/adScriptBehavior.js"  // ../../examples/IAB_RisingStars/Billboard_adfiles/adScriptBehavior.js
 
-
+var html =   "<!-- \nTHIS IS EMBEDDED AS scripted content .\nSource here is provided as a mechanism to make the ad more readable.\nActual source is translated into Javascript string and placed in \"billboardAdBootstrap.js\" file.\n-->\n<style>\n#showAdButton{\n display: none;\n cursor: pointer;\n text-decoration: underline;\n text-align: right;\n width:970px;\n color: #888;\n font-weight:bold;\n font-size:0.8em;\n font-family: Verdana;\n}\n</style>\n\n<scr"+"ipt>\nfunction preCollapse(){\n collapseAd();\n}\n</scr"+"ipt>\n\n<div id=\"showAdButton\" onclick=\"expandAd()\">Show ad <img src=\"https://s3-us-west-2.amazonaws.com/safeframe/samples/rising_stars/billboard_adfiles/downarrow.png\" /></div>\n\n<a href=\"javascript:collapseAd();\" onclick=\"collapseAd();return false;\"><img src=\"https://s3-us-west-2.amazonaws.com/safeframe/samples/rising_stars/billboard_adfiles/backupImage.png\"\n id=\"adImage\"\n style=\"width:970px;height:250px;border:0;\" /></a>\n\n<scr"+"ipt src=\"https://s3-us-west-2.amazonaws.com/safeframe/samples/rising_stars/billboard_adfiles/adScriptBehavior.js\"></scr"+"ipt>\n"
 
 document.write(html);
