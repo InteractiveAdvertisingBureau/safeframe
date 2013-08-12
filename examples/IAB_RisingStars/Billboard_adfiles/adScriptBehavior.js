bd1b26475623f61218361c03f02f51f592e192c5
Chris Cole <goosemanjack@gmail.com>
2013-08-12 09:24:08 -0700

    Rising star sample index page.

--- examples/IAB_RisingStars/Billboard_adfiles/adScriptBehavior.js
+++ examples/IAB_RisingStars/Billboard_adfiles/adScriptBehavior.js
@@ -18,7 +18,7 @@ var sfAPI = extern;
  /**
  * Expand the ad by calling the SafeFrame API
  */
- function expandAd(){
+ function expandAd(dim){
   var g, ex, obj;
 	if ($sf.ext) {
 		   try {
@@ -33,11 +33,21 @@ var sfAPI = extern;
 				g = $sf.ext.geom(); // the geometry object
 				ex = g && g.exp;
 				obj = {};
-				obj.l=0;
-				obj.r=970;
-				obj.t=0;
-				obj.b=250;
-				obj.push=true;
+				
+				if(dim){
+					obj.l=dim.l || 0;
+					obj.r=dim.r || 970;
+					obj.t=dim.t || 0;
+					obj.b=dim.b || 250;
+					obj.push= dim.push || true;
+				}
+				else{				
+					obj.l=0;
+					obj.r=970;
+					obj.t=0;
+					obj.b=250;
+					obj.push=true;
+				}
 				//if (Math.abs(ex.l) >= expandedWidth && Math.abs(ex.t) >= expandedHeight) {
 				  $sf.ext.expand(obj);
 			//}
@@ -78,7 +88,7 @@ var sfAPI = extern;
  setTimeout(function(){
   var body = document.getElementsByTagName('body')[0];
   body.style.height='100%';
-  expandAd();
+  expandAd(window.sampleAdDim);
 
   /*
   body.addEventListener('mouseover', function(){
