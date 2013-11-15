fd9e40eb85cc13368401020b834a6e686a049ad6
Chris Cole <goosemanjack@gmail.com>
2013-11-15 09:16:58 -0800

    Cleanup of Samples. Adding analytics for testing tools on hosted domain.

---
+++ examples/IAB_RisingStars/risingstarnav.js
@@ -0,0 +1,11 @@
+document.write(' \
+<ul class="nav"> \
+		<li><a href="./index.html">Home</a></li> \
+		<li><a href="./billboard_sample.html">Billboard</a></li> \
+		<li><a href="./filmstrip_sample.html">Filmstrip</a></li> \
+		<li><a href="./portrait_sample.html">Portrait</a></li> \
+		<li><a href="./pushdown_sample.html">Pushdown</a></li> \
+		<li><a href="./sidekick_sample.html">Sidekick</a></li> \
+		<li><a class="unavailable" href="#">Slider</a></li> \
+</ul> \
+');
