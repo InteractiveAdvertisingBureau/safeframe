0f92048a406dc804cb5f3e8a9029a4fce833d385
Chris Cole <goosemanjack@gmail.com>
2015-02-05 13:41:17 -0800

    Bootloader sample working.

--- examples/bootload/sf_bootloader.js
+++ examples/bootload/sf_bootloader.js
@@ -12,7 +12,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 "use strict";
 (function(win) {
 	// Root location of the SafeFrame framework. Modify for your distribution
-	var FRAMEWORK_ROOT = 'https://s3-us-west-2.amazonaws.com/safeframe/refimpl/1-0-3/'; //js/lib/base.js
+	var FRAMEWORK_ROOT = 'http://s3-us-west-2.amazonaws.com/safeframe/refimpl/1-0-3/'; //js/lib/base.js
 	
 	// If you minimize the source, modify the HOST_FILES array to specify your pub-side file
 	// and the RENDER_FILE to your custom r.html file
@@ -23,32 +23,234 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 			],
 		RENDER_FILE = FRAMEWORK_ROOT + 'html/r.html';
 	
+	var win = win || window,
+		doc = win.document;
+	
+	var thisScript = null,
+		thisScriptSrc = null,
+		adSpec = {},
+		positions = [];
+	
 	// Stages of the bootloader execution
 	var eventstages = {
+		scriptInjected: false,
 		bootloaded : false,
 		configured : false,
 		rendered: false
 	};
+
+	/**
+	* Utility Functions
+	*/
+	var Utils = {
 	
-	function attachEvt(evt){
-		if(
-	} 
+		makeEl : function (tag, attrs){
+			var i, el;
+			
+			el = doc.createElement(tag);
+			if(attrs && attrs.length > 0 && attrs.length % 2 == 0){
+				for(i=0; i < attrs.length; i=i+2){
+					el.setAttribute(attrs[i], attrs[i+1]);
+				}
+			}
+			return el;
+		},
+        mixin: function (objA, objB) {
+            var key;
+
+            if (objA == null) {
+                return objB;
+            }
+            if (objB == null) {
+                return objB;
+            }
+
+            for (key in objB) {
+                if (objB.hasOwnProperty(key)) {
+                    objA[key] = objB[key];
+                }
+            }
+
+            return objA;
+        },
+		
+		rand: function() { return Math.round(Math.random()*100); },
+		
+		attachEvt: function (elem, event, cb){
+			var ael = 'addEventListener',
+				ae = 'attachEvent';
+			if(elem[ael]){
+				elem[ael](event, cb);
+			}
+			else if(elem[ae]){
+				elem[ae]('on'+event, cb);
+			}
+		},
+		
+		/**
+		* Dynamically load a script into the DOM
+		*/
+		loadScript: function (src){
+			var head = doc.head,
+				script = Utils.makeEl('script', ['src', src]);
+			head.appendChild(script);		
+		},
+		/**
+		* Return parameters in query or fragment as a key value hash
+		*/
+		getParameterHash: function(url){
+			var posA = url.indexOf('?'),
+				posB = url.indexOf('#'),
+				data = {},
+				i, bits, parts, val,
+				str, start;
+			
+			if(posA == -1 && posB == -1){
+				return data;
+			}
+			if(posA == -1 || posB == -1){
+				start = Math.max(posA, posB);
+			}
+			else{
+				start = Math.min(posA, posB);	
+			}
+			
+			str = url.substring(start+1).replace(/[\?#]/g, '&');
+            parts = str.split("&");
+            for (i = 0; i < parts.length; i++) {
+                bits = parts[i].split("=");
+				if(bits.length > 2){
+					val = bits.slice(1).join('=');
+				}
+				else{
+					val = bits[1];
+				}
+                if (bits.length > 1) {
+                    data[bits[0].toLowerCase()] = decodeURIComponent(val);
+                }
+            }
+            return data;		
+		}		
+	}
 	
 	
+	/**
+	* Inject framework scripts
+	*/	
+	function injectScripts (){
+		var i;
+		for(i=0; i < HOST_FILES.length; i++){
+			Utils.loadScript(HOST_FILES[i]);
+		}
+		eventstages.scriptInjected = true;
+	}	
+	
+	
+	function renderPositions(){
+		var params = Utils.getParameterHash(thisScriptSrc),
+			adConfig,
+			targEl,
+			markup,
+			targetId;
+		
+		adConfig = {
+			w : params.w,
+			h : params.h,
+			dest: params.target || null
+		}
+		
+		markup = '<scr' + 'ipt type="text/javascript" src="' + params.ad + '"></scr' +  'ipt>';
+		
+		var makeTarget = function(id){
+			var par = thisScript.parentNode,
+				id = id || 'sf-boot-node_' + Utils.rand();
+			var el;
+			el = Utils.makeEl('div', ['style', 'display:inline-block', 'id', id]);
+			par.insertBefore(el, thisScript);
+			return el;
+		}
+		
+		if(adConfig.dest){
+			targEl = doc.getElementById(adConfig.dest);
+		}
+		
+		if(targEl == null){
+			targEl = makeTarget(adConfig.dest);
+			adConfig.dest = targEl.getAttribute('id');
+		}
+		
+		var posConf = new $sf.host.PosConfig(adConfig);
+		var pos = new $sf.host.Position(posConf, markup);
+		
+		$sf.host.render(pos);
+		
+	}
 	
 	function bootload(){
-		if(win['$sf'] !== undefined && win['$sf'] != null){
+		var conf,
+			hasSf = (win['$sf'] !== undefined && win['$sf'] != null),
+			hasSfLoaded = (hasSf && win['$sf']['host'] != null);
+		
+		if(hasSfLoaded){
+			// Framework already loaded
 			eventstages.bootloaded = true;
 			eventstages.configured = true;
-			
+			var conf = new $sf.host.Config({
+				renderFile: RENDER_FILE,
+				positions: {}
+			});
+		
 			setTimeout(renderPositions, 2);
-		}	
+		}
+		else{
+			if(!hasSf || !eventstages.scriptInjected){
+				injectScripts();
+			}
+			setTimeout(bootload, 2);
+		}
 	}
 
 	function configLoadEvents(){
-	
-	
+		var loadEvent = 'DOMContentLoaded';
+		if(doc['addEventListener']){
+			Utils.attachEvt(doc, loadEvent, bootload);
+		}
+		else{
+			Utils.attachEvt(doc, 'load', bootload);
+		}
 	}
-
+	
+	/**
+	* Tracks down our current script to pull off query or fragment parameters
+	*/
+	function findThisScriptElem(){
+		var scripts, src, i;
+		
+		if(thisScript != null){
+			return thisScript;
+		}
+		if(doc['currentScript']){
+			thisScript = doc['currentScript'];
+			thisScriptSrc = thisScript.getAttribute('src');
+		}
+		else{
+			scripts = document.getElementsByTagName('script');
+			for(i = scripts.length -1; i >= 0; i--){
+				src = scripts[i].getAttribute('src');
+				if(src && src.indexOf('sf_bootloader') > -1){
+					thisScript = scripts[i];
+					thisScriptSrc = src;
+				}
+			}
+		}
+		
+		return thisScript;
+	}
+	
+	// Initialize function
+	(function (){
+		findThisScriptElem();
+		configLoadEvents();
+	})();
 
 })(window);
