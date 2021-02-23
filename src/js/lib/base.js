9eb96173e5c58f26d2a53334acd84ba86811505b
Alexandre Desilets-Benoit <alexandre.desilets-benoit@contxtful.com>
2021-02-23 14:02:48 -0500

    fix(base.js): remove redundant argument

--- src/js/lib/base.js
+++ src/js/lib/base.js
@@ -2217,7 +2217,6 @@ if (window["$sf"]) {
 			attr(newCl,"TABINDEX",-1);
 			attr(newCl,"MARGINWIDTH",0);
 			attr(newCl,"MARGINHEIGHT",0);
-			attr(newCl,"MARGINHEIGHT",0);
 			attr(newCl,"allow","accelerometer; gyroscope;");
 
 			_bind_iframe_onload(newCl,cb);
